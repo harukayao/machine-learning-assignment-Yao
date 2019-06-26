895d36211c222a093852a5150916d42d18ed4230
Yao <31342389+harukayao@users.noreply.github.com>
2019-06-26 09:12:34 +0800

    Add files via upload

---
+++ ex1/gradientDescent.py
@@ -0,0 +1,44 @@
+import numpy as np
+from computeCost import *
+
+
+def gradient_descent(X, y, theta, alpha, num_iters):
+    # Initialize some useful values
+    m = y.size
+    J_history = np.zeros(num_iters)
+    temp = np.zeros(len(theta))
+    for i in range(0, num_iters):
+        # ===================== Your Code Here =====================
+        # Instructions : Perform a single gradient step on the parameter vector theta
+        #
+        # Hint: X.shape = (97, 2), y.shape = (97, ), theta.shape = (2, )
+
+
+        # ===========================================================
+        # Save the cost every iteration
+        J_history[i] = compute_cost(X, y, theta)
+        diff = np.matmul(X, theta) - y
+        for j in range(len(theta)):
+            temp[j] = np.sum(np.multiply(diff, X[:,j]))
+            
+        theta = theta - (alpha / m) * temp
+
+    return theta, J_history
+
+
+def gradient_descent_multi(X, y, theta, alpha, num_iters):
+    # Initialize some useful values
+    m = y.size
+    J_history = np.zeros(num_iters)
+
+    for i in range(0, num_iters):
+        # ===================== Your Code Here =====================
+        # Instructions : Perform a single gradient step on the parameter vector theta
+        #
+
+
+        # ===========================================================
+        # Save the cost every iteration
+        J_history[i] = compute_cost(X, y, theta)
+
+    return theta, J_history
---
+++ ex1/plotData.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+
+def plot_data(x, y):
+    # ===================== Your Code Here =====================
+    # Instructions : Plot the training data into a figure using the matplotlib.pyplot
+    #                using the "plt.scatter" function. Set the axis labels using
+    #                "plt.xlabel" and "plt.ylabel". Assume the population and revenue data
+    #                have been passed in as the x and y.
+
+    # Hint : You can use the 'marker' parameter in the "plt.scatter" function to change the marker type (e.g. "x", "o").
+    #        Furthermore, you can change the color of markers with 'c' parameter.
+
+
+    # ===========================================================
+    plt.scatter(x, y, s=10, c='r', marker='x')
+    plt.ylabel('Profit in $10,000s')
+    plt.xlabel('Population of City in 10,000s')
+
